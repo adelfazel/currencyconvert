70a83c0bdc5dae4b294d8968fd95c67bcfee2850
Adelfazel <adel.fazel86@gmail.com>
2019-01-24 14:22:25 +1100

    still making things sync

--- application.py
+++ application.py
@@ -1,21 +1,6 @@
 from flask import Flask, render_template, request
-
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
-    return "Hello World"
-
-
-@app.route("/names")
-def output_names():
-    all_names = set(["Hassan", "Kasim"])
-    return render_template("index.html", all_names=all_names)
-
-
-notes = []
-
-@app.route("/notes", methods = ["POST","GET"])
-def output_names():
-    return render_template("index.html", all_names=all_names)
+    return render_template("index.html")
